054211020599f8069866cc403226f174faf7ac9c
JeongHwan <wjdghks0458@gmail.com>
2022-02-14 11:36:57 +0900

    Clean Code

--- Multithread-tcp-server/server.c
+++ Multithread-tcp-server/server.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
-#include<pthread.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <time.h>
 #include <sqlite3.h>
 
 #define PORT 3000
-#define Q_MAX_SIZE  30000
+#define Q_MAX_SIZE 30000
 #define BUF_SIZE 1000
 #define MAX_CLNT 256
 #define MAC_LEN 18
-
+//number and arrangement of clients
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
-
 pthread_mutex_t mutex;
 
 typedef struct _item_t {
@@ -35,7 +34,7 @@ typedef struct __circleQueue {
 
 Queue sensor_data_q[Q_MAX_SIZE];
 
-void InitQueue(Queue * q) 
+void InitQueue(Queue * q)
 {
     q -> front = 0;
     q -> rear = 0;
@@ -43,37 +42,38 @@ void InitQueue(Queue * q)
     memset(q->chip_number, 0, MAC_LEN*sizeof(q->chip_number[0]));
 }
 
-int IsEmpty(Queue * q) 
+int IsEmpty(Queue * q)
 {
-    if (q -> front == q -> rear) //front와 rear가 같으면 큐는 비어있는 상태
+    if (q -> front == q -> rear) { //If front and rear are the same, the queue is empty.
         return 1;
-    else 
+    }
+    else {
         return 0;
+    }
 }
 
-int IsFull(Queue * q) 
+int IsFull(Queue * q)
 {
-    if ((q -> rear + 1) % Q_MAX_SIZE == q -> front)
+    if ((q -> rear + 1) % Q_MAX_SIZE == q -> front) { //If front and rear + 1 are the same, the queue is full.
         return 1;
+    }
     return 0;
 }
 
-void AddQ(Queue * q, item_t value) 
+void AddQ(Queue * q, item_t value)
 {
-    if (IsFull(q)) 
-    {
+    if (IsFull(q)) {
         printf("Queue is Full.\n");
         return;
     } 
-    else 
-    {
+    else {
         q -> rear = (q -> rear + 1) % Q_MAX_SIZE;
         q -> data[q -> rear] = value;
     }
     return;
 }
 
-item_t DeleteQ(Queue * q) 
+item_t DeleteQ(Queue * q)
 {
     q -> front = (q -> front + 1) % Q_MAX_SIZE;
     return q->data[q->front];
@@ -82,15 +82,13 @@ item_t DeleteQ(Queue * q)
 static int TryReceive(const int sock, item_t *data, size_t max_len)
 {
     int p_len = 0;
-    while(max_len != p_len)
-    {
+    while (max_len != p_len) {
         int len = recv(sock, data + p_len, max_len, 0);
-        if (len < 0) 
-        {
-            if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK)
+        if (len < 0) {
+            if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK) {
                 len = 0;   // Not an error
-            if (errno == ENOTCONN) 
-            {
+            }
+            if (errno == ENOTCONN) {
                 printf("[sock=%d]: Connection closed", sock);
                 return -2;  // Socket has been disconnected
             }
@@ -103,15 +101,13 @@ static int TryReceive(const int sock, item_t *data, size_t max_len)
 static int MacReceive(const int sock, char *data, size_t max_len)
 {
     int p_len = 0;
-    while(max_len != p_len)
-    {
+    while (max_len != p_len) {
         int len = recv(sock, data + p_len, max_len, 0);
-        if (len < 0) 
-        {
-            if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK)
+        if (len < 0) {
+            if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK) {
                 len = 0;   // Not an error
-            if (errno == ENOTCONN) 
-            {
+            }
+            if (errno == ENOTCONN) {
                 printf("[sock=%d]: Connection closed", sock);
                 return -2;  // Socket has been disconnected
             }
@@ -124,11 +120,9 @@ static int MacReceive(const int sock, char *data, size_t max_len)
 static int SocketSend(const int sock, const char * data, const size_t len)
 {
     int to_write = len;
-    while (to_write > 0)
-    {
+    while (to_write > 0) {
         int written = send(sock, data + (len - to_write), to_write, 0);
-        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK) 
-        {
+        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK) {
             printf("Error occurred during sending");
             return -1;
         }
@@ -140,12 +134,9 @@ static int SocketSend(const int sock, const char * data, const size_t len)
 
 void PushData(int sock, item_t * data, int len) 
 {
-    if (len > 0) 
-    {
-        for (int i = 0; i < len / sizeof(data[0]); i ++) 
-        {
-            if (data[i].gas < 4096 && data[i].mic < 4096) 
-            {
+    if (len > 0) {
+        for (int i = 0; i < len / sizeof(data[0]); i ++) {
+            if (data[i].gas < 4096 && data[i].mic < 4096) {
                 pthread_mutex_lock(& mutex);
                 AddQ(&sensor_data_q[sock], data[i]);
                 pthread_mutex_unlock(& mutex);
@@ -154,44 +145,38 @@ void PushData(int sock, item_t * data, int len)
         // Received some data -> echo back
         static const char * message = "OK client";
         len = SocketSend(sock, message, strlen(message));
-        if (len < 0) // Error occurred on write to this socket -> close it and mark invalid
-        { 
+        if (len < 0) { // Error occurred on write to this socket -> close it and mark invalid
             printf("[sock=%d]: SocketSend() returned %d -> closing the socket", sock, len);
             close(sock);
             sock = -1;
         } 
-        else    // Successfully echoed to this socket
-        {
+        else { // Successfully echoed to this socket
+        
             printf("[sock=%d]: Written %.*s\n", sock, len, message);
         }
     }
 }
 
-void *DataSave(void* arg) 
+void *DataSave(void* arg)
 {
 	int i = 0;
     sqlite3 *db;
     char *err_msg = 0;
-
     int clnt_sock = *((int*)arg);
-
     int rc = sqlite3_open("test.db", &db);
-    if (rc != SQLITE_OK)
-    {
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
     }
 
-	while(1) 
-	{
+	while(1) {
 		
-		if(!IsEmpty(&sensor_data_q[clnt_sock]))
-		{
+		if(!IsEmpty(&sensor_data_q[clnt_sock])) {
             sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
-            for(int i = 1; i < 100; i++)
-            {
-                if(IsEmpty(&sensor_data_q[clnt_sock]))
+            for(int i = 1; i < 100; i++) {
+                if(IsEmpty(&sensor_data_q[clnt_sock])) {
                     break;
+                }
                 pthread_mutex_lock(&mutex);
 			    item_t array = DeleteQ(&sensor_data_q[clnt_sock]);
                 pthread_mutex_unlock(&mutex);
@@ -203,12 +188,9 @@ void *DataSave(void* arg)
                 rc = sqlite3_exec(db, mic, 0, 0, &err_msg);
             }
             sqlite3_exec(db, "END TRANSACTION;", NULL, NULL, NULL);
-            
 		}
-		
 	}
     sqlite3_close(db);
-    
 }
 
 void* DataRead(void* arg)
@@ -216,37 +198,29 @@ void* DataRead(void* arg)
     pthread_t thread;
     int clnt_sock = *((int*)arg);
     int flags = fcntl(clnt_sock, F_GETFL);
-    if (fcntl(clnt_sock, F_SETFL, flags | O_NONBLOCK) == -1)
+    if (fcntl(clnt_sock, F_SETFL, flags | O_NONBLOCK) == -1) {
         printf("Unable to set socket non blocking\n");
+    }
     InitQueue(&sensor_data_q[clnt_sock]);
     
     item_t *recv_buffer;
     recv_buffer = (item_t *)malloc(sizeof(item_t) * (BUF_SIZE-1));
     int len = 0;
-    
-
-    len = MacReceive(clnt_sock, sensor_data_q[clnt_sock].chip_number, sizeof(q[clnt_sock].chip_number));
-
+    len = MacReceive(clnt_sock, sensor_data_q[clnt_sock].chip_number, sizeof(sensor_data_q[clnt_sock].chip_number));
     printf("%s\n", sensor_data_q[clnt_sock].chip_number);
-
     int thr_id = pthread_create(&thread, NULL, DataSave,(void*)&clnt_sock);
-	if (thr_id < 0)
-    {
+	if (thr_id < 0) {
         perror("thread create error : ");
         exit(0);
     }
     pthread_detach(thread);
-
-    while((len=TryReceive(clnt_sock,recv_buffer, sizeof(item_t) * (BUF_SIZE-1))) != -2)
+    while ((len=TryReceive(clnt_sock,recv_buffer, sizeof(item_t) * (BUF_SIZE-1))) != -2) {
         PushData(clnt_sock,recv_buffer, len);
-        
+    }
     pthread_mutex_lock(&mutex);
-    for(int i = 0; i<clnt_cnt; i++)
-    {
-        if(clnt_sock == clnt_socks[i])
-        {
-            while(i<clnt_cnt-1)
-            {
+    for(int i = 0; i<clnt_cnt; i++) {
+        if(clnt_sock == clnt_socks[i]) {
+            while(i<clnt_cnt-1) {
                 clnt_socks[i] = clnt_socks[i+1];
                 ++i;
             }
@@ -256,7 +230,6 @@ void* DataRead(void* arg)
     --clnt_cnt;
     pthread_mutex_unlock(&mutex);
     close(clnt_sock);
-
     return NULL;
 }
 
@@ -266,24 +239,21 @@ int main()
 {
     sqlite3 *db;
     char *err_msg = 0;
-    char *sql = "DROP TABLE IF EXISTS Gas_Datas;" 
+    char *sql = "DROP TABLE IF EXISTS Gas_Datas;"
                 "DROP TABLE IF EXISTS Mic_Datas;"
                 "CREATE TABLE Gas_Datas(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,data INT,sock CHAR(18),TIME DATETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime')));"
                 "CREATE TABLE Mic_Datas(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,data INT,sock CHAR(18),TIME DATETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime')));";
     
-    int rc = sqlite3_open("test.db", &db);    
-    if (rc != SQLITE_OK)
-    {
+    int rc = sqlite3_open("test.db", &db);
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
     }
-
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-    if (rc != SQLITE_OK )
-    {
+    if (rc != SQLITE_OK ) {
         fprintf(stderr, "SQL error: %s\n", err_msg);
 
-        sqlite3_free(err_msg);        
+        sqlite3_free(err_msg);
         sqlite3_close(db);
     }
 
@@ -302,22 +272,24 @@ int main()
 
 	struct sockaddr_in listen_addr;
     
-    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
         printf("socket error\n");
+    }
 
     memset(&listen_addr, 0, sizeof(listen_addr));
     listen_addr.sin_family = AF_INET;
     listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     listen_addr.sin_port = htons(PORT);
 
-    if(bind(listen_sock, (struct sockaddr*)&listen_addr, sizeof(listen_addr)) == -1)
+    if(bind(listen_sock, (struct sockaddr*)&listen_addr, sizeof(listen_addr)) == -1) {
         printf("bind error\n");  
+    }
 
-    if(listen(listen_sock, 1) != 0)
+    if(listen(listen_sock, 1) != 0) {
         printf("listen error\n");
+    }
 
-    while(1)
-    {
+    while(1) {
         struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
         socklen_t addr_len = sizeof(source_addr);
         
@@ -332,4 +304,3 @@ int main()
         pthread_detach(t_id);
     }
 }
-
--- main/non_blocking_socket.c
+++ main/non_blocking_socket.c
@@ -1,26 +1,28 @@
+//This code is a TCP/IP high-speed sensing program targeting the ESP32 board. 
+//It is designed as non-blocking and sends the board's unique MAC address and 
+//sensing data to the server in the form of a structure.
+
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "sys/socket.h"
-#include "netdb.h"
+#include "driver/adc.h"
+#include "driver/gpio.h"
 #include "errno.h"
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
-#include "driver/adc.h"
 #include "esp_adc_cal.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "netdb.h"
 #include "protocol_examples_common.h"
+#include "sys/socket.h"
 
 #define INVALID_SOCK (-1)
 #define YIELD_TO_ALL_MS 50
-
 #define DEFAULT_VREF 1100
 #define MAX_SIZE 1000
-
 static esp_adc_cal_characteristics_t * adc_chars;
 static const adc_channel_t gas_channel = ADC_CHANNEL_6; // GPIO34 if gas, GPIO35 if mic
 static const adc_channel_t mic_channel = ADC_CHANNEL_7;
@@ -30,20 +32,22 @@ static const adc_unit_t unit = ADC_UNIT_1;
 
 SemaphoreHandle_t xmutex;
 
-typedef struct _item_t {
+typedef struct _item_t 
+{
     uint16_t mic;
     uint16_t gas;
 }item_t;
 
-
-typedef struct __circleQueue {
+typedef struct __circleQueue 
+{
     int rear;
     int front;
     item_t * data;
     item_t * array_to_send;
 }Queue;
 
-Queue q;
+//Circular queue for data storage
+Queue data_q;
 
 void InitQueue(Queue * q) 
 {
@@ -52,100 +56,83 @@ void InitQueue(Queue * q)
     q->data = (item_t *)pvPortMalloc(sizeof(item_t) * MAX_SIZE);
     q->array_to_send = (item_t *)pvPortMalloc(sizeof(item_t) * (MAX_SIZE));
 }
+
 int IsEmpty(Queue * q) 
 {
-    if (q -> front == q -> rear)  // front와 rear가 같으면 큐는 비어있는 상태
-        return true;
-    else 
-        return false;
-    
-}
-int IsFull(Queue * q) 
-{
-    if ((q -> rear + 1) % MAX_SIZE == q -> front) 
-    {
+    if (q -> front == q -> rear) {  //If front and rear are the same, the queue is empty.
         return true;
     }
+    else {
+        return false;
+    } 
+}
+
+int IsFull(Queue * q) 
+{
+    if ((q -> rear + 1) % MAX_SIZE == q -> front) { //If front and rear + 1 are the same, the queue is full.
+        return true;
+    }
+        
     
     return false;
 }
-void AddQ(Queue * q, item_t value) 
+
+void AddQ(Queue * q, item_t value)
 {
-    if (IsFull(q)) 
-    {
+    if (IsFull(q)) {
         printf("Queue is Full.\n");
         return;
     } 
-    else 
-    {
+    else {
         q -> rear = (q -> rear + 1) % MAX_SIZE;
         q -> data[q -> rear] = value;
     }
     return;
 }
-item_t DeleteQ(Queue * q) 
+
+item_t DeleteQ(Queue * q)
 {
     q -> front = (q -> front + 1) % MAX_SIZE;
     return q -> data[q -> front];
 }
-void AllPopQ(Queue * q) 
+
+void AllPopQ(Queue * q)
 {
-    for (int i = 0; i < MAX_SIZE - 1; i ++) 
+    for (int i = 0; i < MAX_SIZE - 1; i ++) {
         q -> array_to_send[i] = DeleteQ(q);
+    }
 }
 
-static void CheckEfuse(void) 
+static void CheckEfuse(void)
 {
     // Check if TP is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) 
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
         printf("eFuse Two Point: Supported\n");
-    else 
+    }
+    else {
         printf("eFuse Two Point: NOT supported\n");
+    }
     // Check Vref is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) 
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
         printf("eFuse Vref: Supported\n");
-    else
+    } 
+    else {
         printf("eFuse Vref: NOT supported\n");
+    }
 }
 
 static void PrintCharValType(esp_adc_cal_value_t val_type) {
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
         printf("Characterized using Two Point Value\n");
-    else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) 
-        printf("Characterized using eFuse Vref\n");
-    else
-        printf("Characterized using Default Vref\n");
-}
-
-void AdcRead() 
-{
-    while (1) 
-    {
-        if (xmutex != NULL) 
-        {
-            if (IsEmpty(&q)) 
-            {
-                while(!IsFull(&q))
-                {
-                    uint16_t gas_value, mic_value;
-                    // Multisampling
-                    gas_value = adc1_get_raw((adc1_channel_t)gas_channel);
-                    mic_value = adc1_get_raw((adc1_channel_t)mic_channel);
-                    item_t item;
-                    item.gas = gas_value;
-                    item.mic = mic_value;
-                    if (xSemaphoreTake(xmutex, portMAX_DELAY) == pdTRUE) 
-                    {
-                        AddQ(&q, item);
-                        xSemaphoreGive(xmutex);
-                    }
-
-                }
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(10));
     }
-    vTaskDelete(NULL);
+        
+    else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    }
+        
+    else {
+        printf("Characterized using Default Vref\n");
+    } 
 }
 
 static void LogSocketError(const char *tag, const int sock, const int err, const char *message)
@@ -157,30 +144,26 @@ static void LogSocketError(const char *tag, const int sock, const int err, const
 static int TryReceive(const char *tag, const int sock, char * data, size_t max_len)
 {
     int len = recv(sock, data, max_len, 0);
-    if (len < 0) 
-    {
-        if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK)
+    if (len < 0) {
+        if (errno == EINPROGRESS || errno == EAGAIN || errno == EWOULDBLOCK) {
             return 0;   // Not an error
-        if (errno == ENOTCONN) 
-        {
+        } 
+        if (errno == ENOTCONN) {
             ESP_LOGW(tag, "[sock=%d]: Connection closed", sock);
             return -2;  // Socket has been disconnected
         }
         LogSocketError(tag, sock, errno, "Error occurred during receiving");
         return -1;
     }
-
     return len;
 }
 
 static int SocketSend(const char *tag, const int sock, Queue *q, int len)
 {
     int to_write = len;
-    while (to_write > 0) 
-    {
+    while (to_write > 0) {
         int written = send(sock, q->array_to_send + (len - to_write), to_write, 0);
-        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK) 
-        {
+        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK) {
             LogSocketError(tag, sock, errno, "Error occurred during sending");
             return -1;
         }
@@ -192,11 +175,9 @@ static int SocketSend(const char *tag, const int sock, Queue *q, int len)
 static int MacAddressSend(const char *tag, const int sock, char *str, int len)
 {
     int to_write = len;
-    while (to_write > 0) 
-    {
+    while (to_write > 0) {
         int written = send(sock, str + (len - to_write), to_write, 0);
-        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK)
-        {
+        if (written < 0 && errno != EINPROGRESS && errno != EAGAIN && errno != EWOULDBLOCK) {
             LogSocketError(tag, sock, errno, "Error occurred during sending");
             return -1;
         }
@@ -205,107 +186,121 @@ static int MacAddressSend(const char *tag, const int sock, char *str, int len)
     return len;
 }
 
+void AdcRead()
+{
+    while (1) {
+        if (xmutex != NULL) {
+            if (IsEmpty(&data_q)) {
+                while (!IsFull(&data_q)) {
+                    uint16_t gas_value, mic_value;
+                    // Multisampling
+                    gas_value = adc1_get_raw((adc1_channel_t)gas_channel);
+                    mic_value = adc1_get_raw((adc1_channel_t)mic_channel);
+                    item_t item;
+                    item.gas = gas_value;
+                    item.mic = mic_value;
+                    if (xSemaphoreTake(xmutex, portMAX_DELAY) == pdTRUE) {
+                        AddQ(&data_q, item);
+                        xSemaphoreGive(xmutex);
+                    }
+
+                }
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    vTaskDelete(NULL);
+}
+
 static void DataSent(void *pv_parameters)
 {
     static const char *TAG = "nonblocking-socket-client";
     static char rx_buffer[128];
-
     struct addrinfo hints = { .ai_socktype = SOCK_STREAM };
     struct addrinfo *address_info;
     int sock = INVALID_SOCK;
+    int res = getaddrinfo(CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_ADDRESS,
+                         CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_PORT,
+                         &hints,
+                         &address_info);
 
-    int res = getaddrinfo(CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_ADDRESS, CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_PORT, &hints, &address_info);
-    if (res != 0 || address_info == NULL)
+    if (res != 0 || address_info == NULL) {
         ESP_LOGE(TAG, "couldn't get hostname for `%s` "
                       "getaddrinfo() returns %d, addrinfo=%p", CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_ADDRESS, res, address_info);
+    }
 
-    while(1)
-    {
+    while (1) {
         // Creating client's socket
         sock = socket(address_info->ai_family, address_info->ai_socktype, address_info->ai_protocol);
-        if (sock < 0) 
+        if (sock < 0) {
             LogSocketError(TAG, sock, errno, "Unable to create socket");
-        ESP_LOGI(TAG, "Socket created, connecting to %s:%s", CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_ADDRESS, CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_PORT);
-
+        }
+        ESP_LOGI(TAG, "Socket created, connecting to %s:%s",
+                CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_ADDRESS,
+                CONFIG_EXAMPLE_TCP_CLIENT_CONNECT_PORT);
         // Marking the socket as non-blocking
         int flags = fcntl(sock, F_GETFL);
-        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1)
+        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
             LogSocketError(TAG, sock, errno, "Unable to set socket non blocking");
-
-        if (connect(sock, address_info->ai_addr, address_info->ai_addrlen) != 0) 
-        {
-            if (errno == EINPROGRESS) 
-            {
+        }  
+        if (connect(sock, address_info->ai_addr, address_info->ai_addrlen) != 0) {
+            if (errno == EINPROGRESS) {
                 ESP_LOGD(TAG, "connection in progress");
                 fd_set fdset;
                 FD_ZERO(&fdset);
                 FD_SET(sock, &fdset);
-
                 // Connection in progress -> have to wait until the connecting socket is marked as writable, i.e. connection completes
                 res = select(sock+1, NULL, &fdset, NULL, NULL);
-                if (res < 0) 
+                if (res < 0) {
                     LogSocketError(TAG, sock, errno, "Error during connection: select for socket to be writable");
-                else if (res == 0)
+                }
+                else if (res == 0) {
                     LogSocketError(TAG, sock, errno, "Connection timeout: select for socket to be writable");
+                }
                 else {
                     int sockerr;
                     socklen_t len = (socklen_t)sizeof(int);
-
-                    if (getsockopt(sock, SOL_SOCKET, SO_ERROR, (void*)(&sockerr), &len) < 0)
+                    if (getsockopt(sock, SOL_SOCKET, SO_ERROR, (void*)(&sockerr), &len) < 0) {
                         LogSocketError(TAG, sock, errno, "Error when getting socket error using getsockopt()");
-                    if (sockerr)
+                    }
+                    if (sockerr) {
                         LogSocketError(TAG, sock, sockerr, "Connection error");
+                    } 
                 }
             } 
-            else
+            else {
                 LogSocketError(TAG, sock, errno, "Socket is unable to connect");
+            }         
         }
-
         int optval = 2;
-
         setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof (optval));
-
         uint8_t mac[6];
-
         esp_efuse_mac_get_default(mac);
-
         char macStr[18] = { 0 };
-
         sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-        printf("%s\n", macStr);
-
         int len = MacAddressSend(TAG, sock, macStr, sizeof(macStr));
-        if (len < 0) 
-        {
+        if (len < 0) {
             ESP_LOGE(TAG, "Error occurred during SocketSend");
             break;
         }
 
-        while(1)
-        {
+        while (1) {
             vTaskDelay(pdMS_TO_TICKS(10));
-            if(xmutex != NULL)
-            {
-                if(IsFull(&q))
-                {
-                    if(xSemaphoreTake(xmutex, portMAX_DELAY) == pdTRUE)
-                    {
-                        AllPopQ(&q);
+            if (xmutex != NULL) {
+                if (IsFull(&data_q)) {
+                    if (xSemaphoreTake(xmutex, portMAX_DELAY) == pdTRUE) {
+                        AllPopQ(&data_q);
                         xSemaphoreGive(xmutex);
                     }
-                    int len = SocketSend(TAG, sock, &q, sizeof(item_t) * (MAX_SIZE-1));
-                    if (len < 0) 
-                    {
+                    int len = SocketSend(TAG, sock, &data_q, sizeof(item_t) * (MAX_SIZE-1));
+                    if (len < 0) {
                         ESP_LOGE(TAG, "Error occurred during SocketSend");
                         break;
                     }
                     //Keep receiving until we have a reply
-                    do 
-                    {
+                    do {
                         len = TryReceive(TAG, sock, rx_buffer, 9);
-                        if (len < 0) 
-                        {
+                        if (len < 0) {
                             ESP_LOGE(TAG, "Error occurred during TryReceive");
                             break;
                         }
@@ -319,7 +314,6 @@ static void DataSent(void *pv_parameters)
         }
         close(sock);
     }
-
     free(address_info);
     vTaskDelete(NULL);
 }
@@ -331,7 +325,6 @@ void app_main(void)
     adc1_config_width(width);
     adc1_config_channel_atten(gas_channel, atten);
     adc1_config_channel_atten(mic_channel, atten);
-
     //Characterize ADC
     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(
@@ -341,19 +334,14 @@ void app_main(void)
         DEFAULT_VREF,
         adc_chars
     );
-
     PrintCharValType(val_type);
-
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
-    InitQueue(&q);
-
+    InitQueue(&data_q);
     xmutex = xSemaphoreCreateMutex();
-
     xTaskCreate(AdcRead, "AdcRead", 4096, NULL, 5, NULL);
     xTaskCreate(DataSent, "DataSent", 4096, NULL, 5, NULL);
-
 }
